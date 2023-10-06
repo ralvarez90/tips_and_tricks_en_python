33bec8b4ec5de76628f9f4ff03ce7609ff7914e1
Rodrigo Alvarez <rodrigo.alvare.herrera@gmail.com>
2023-10-06 16:45:40 -0600

    Clase Counter, metodos most_common y get

---
+++ _07_conteo_de_ocurrencias.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from collections import Counter
+
+"""COUNTING ITEM OCURRENCES
+
+Si desea saber el número de veces que un determinado elemento
+aparece o se encuentra dentro de un iterable, se puede emplear la
+clase Counter que retorna un diccionario de cuantos elementos 
+aparecen en en un iterable.
+
+Cada key del diccionario resultante es un elemento del
+iterable.
+
+Notas:
+- Los objetos Counter extienden de dict, y tienen un método
+llamado most_common que permiten obteneruna lista con los
+n elementos más comunes.
+"""
+
+# ejemplo 1, lista de strings
+lista1 = ['Juan', 'Hugo', 'Paco', 'Luis', 'Paco', 'Ramiro']
+contador = Counter(lista1)
+print(f'Conteo en lista1:\n{contador}')
+print(f'"Paco" aparece en la lista {contador.get("Paco")} veces\n')
+
+# ejemplo 2, string
+some_text = 'Esto es un menesaje.'
+print(f'some_text: "{some_text}"')
+letter_counter = Counter(some_text.lower())
+print(f'Conteo en some_text:\n{letter_counter}')
+print(f'3 letras más comunes: {letter_counter.most_common(3)}')
