20800d0bfdf1e3089dda9a1e5fd8f69233e33a9d
Rodrigo Alvarez <rodrigo.alvare.herrera@gmail.com>
2023-10-07 02:21:11 -0600

    Uso de help('keywords')

---
+++ _15_palabras_reservadas.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+"""PALABRAS RESERVADAS
+
+Empleamos la función help con el string 'keywords' para obtener la lista
+de palabras reservadas del lenguaje."""
+help('keywords')
