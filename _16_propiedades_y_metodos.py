0cf123b05cda02d990c68e45aee047a37d1679a8
Rodrigo Alvarez <rodrigo.alvare.herrera@gmail.com>
2023-10-07 02:29:55 -0600

    Listar propiedades y metodos don dir

---
+++ _16_propiedades_y_metodos.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""PROPIEDADES Y MÉTODOS
+
+Empleamos la función dir y retorna las propiedaes y métodos de un
+objetos. Retorna una lsita de strings."""
+
+some_string = 'this is a string'
+print(dir(some_string))
