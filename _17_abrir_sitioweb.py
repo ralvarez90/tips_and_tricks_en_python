1cdf205b320a55a82d14f8a934e479b3dee094b5
Rodrigo Alvarez <rodrigo.alvare.herrera@gmail.com>
2023-10-07 02:30:51 -0600

    Abrir sitio web usando webbrowser.open o webbrowser.new_tab

---
+++ _17_abrir_sitioweb.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+import webbrowser
+
+"""ABRIR SITIO WEB
+
+Usando el paquete webbrowser podemos abrir directamente un 
+sitio web."""
+
+url = 'www.google.com.mx'
+open_web = webbrowser.open(url, 2)
