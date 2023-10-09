49adf04e0928902dcdb409aca762cdc1afc9cb91
Rodrigo Alvarez <rodrigo.alvare.herrera@gmail.com>
2023-10-09 16:57:47 -0600

    Localizacion de paquetes imprimiendo la libreria importando con print

---
+++ _52_localizacion_de_bibliotecas.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import pandas
+import sys
+
+"""LOCALIZACIÓN DE BIBLOTECLAS
+
+Al importar un paquete o módulo en python, si lo
+imprimimos con un print se obtiene la ruta donde
+dicho módulo/paquete está instalado."""
+print(pandas)
+print(sys)
