66ec5f0eca3f11f2235371bb74a9cb976bf1c220
Rodrigo Alvarez <rodrigo.alvare.herrera@gmail.com>
2023-10-09 00:30:23 -0600

    Mostrar el zen de python con el modulo this

---
+++ _44_el_zen_de_python.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+import this
+
+"""EL ZEN DE PYTHON
+
+El zen de python es una lista de 19 principios guía de escritura
+de código python. Se pueden visualizar importando el módulo this.
+
+Pude almacenarlo en un archivo de la forma
+python3 script_this.py > zen_of_python.txt donde script_this.py 
+importa this y zen_of_python.txt el el fichero de texto al cuañ
+se le redirecciona la salida."""
